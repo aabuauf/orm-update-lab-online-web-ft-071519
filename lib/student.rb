c07b97ca25d84db8e95bc7d2d228cfc099ad535c
Ahmed Abououf <aabuauf@yahoo.com>
2019-08-15 11:51:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -59,6 +59,14 @@ class Student
     student = Student.new(row[1],row[2],row[0])
   end
   
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM students WHERE name = ?
+    SQL
+    
+    DB[:conn].execute(sql,name)
+    binding.pry
+  end
 end
 
 

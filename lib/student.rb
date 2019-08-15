f2d9ec62cbeab070d21d424f0f7559adee0b9f90
Ahmed Abououf <aabuauf@yahoo.com>
2019-08-15 11:55:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -64,7 +64,7 @@ class Student
       SELECT * FROM students WHERE name = ?
     SQL
     
-    Student.new_from_db(DB[:conn].execute(sql,name))
+    Student.new_from_db(DB[:conn].execute(sql,name)[0])
 
   end
 end
